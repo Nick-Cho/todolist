cd47578ffc7f89948cf0d93408d184b928b8baa1
Nick Cho <nicholas.cho@hotmail.ca>
2021-01-03 14:01:33 -0500

    Formatting Completed Todo's

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 export class TodoItem extends Component {
   isCompleted = () => {
-    return (this.props.todo.completed ? {textDecoration: 'line-through'} : {textDecoration: 'none'})
+    return {
+      boxSizing: 'borderBox',
+      border: '0.5px #d3d3d3 solid', 
+      padding: '10px',
+      margin: '5px',
+      marginLeft: '525px',
+      textDecoration: this.props.todo.completed ? 'line-through': 'none',
+      width: '800px'
+    }
   }
   render() {
     return (
-      <div style = {this.isCompleted()}>
-        <p>{this.props.todo.title}</p>
+      <div style = {this.isCompleted()}> 
+        <p>
+          <input type = "checkbox" onChange = {this.markCompleted}/> {' '}
+          {this.props.todo.title}
+        </p>
       </div>
     )
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 class Todos extends Component {
+  markCompleted = (event)=>{
+
+  }
   render(todos){
     return this.props.todos.map((todo) => (
-      <TodoItem key = {todo.id} todo = {todo}/>
+      <TodoItem key = {todo.id} todo = {todo} markCompleted = {this.markCompleted}/>
     ));
   }
 }
