f571f15ca46e1c042c7545ae28048a0883adfafe
Nick Cho <65980644+Nick-Cho@users.noreply.github.com>
2021-01-05 12:40:07 -0500

    Created Form to Add Todo's

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 export class AddTodo extends Component {
+  state = {
+    title : ''
+  }
+
+  onChange = (e) => this.setState({[e.target.name]: e.target.value});
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({title: ''});
+  }
   render() {
     return (
       <div>
-        
+        <form onSubmit = {this.onSubmit} style = {{display: 'flex', width: '770px', padding: '0 36.4rem'}}>
+          <input 
+          type = "text" 
+          name = "title" 
+          placeholder = "Add Todo"
+          style = {{flex: '10', padding: '5px'}}
+          value = {this.state.title}
+          onChange = {this.onChange}
+          />
+
+          <input 
+          type = "submit" 
+          value = "Add" 
+          className = "btn" 
+          style = {{flex: '1'}}/>
+        </form>
       </div>
     )
   }
 }
+
 
 export default AddTodo
